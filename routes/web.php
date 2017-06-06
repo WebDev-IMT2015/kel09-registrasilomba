6613ac0895732e25f9f1538109be780962aea28d
SubaruYashiro <michaell.randicha@gmail.com>
2017-06-06 19:09:33 +0700

    Michael : Multiple Authentication (Admin and User) On Progress
    Multiple Authentication still on progress

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+Route::group(['middleware' => ['guest']], function () {
+    
+    Route::group(['prefix' => '/'], function(){
+        Route::get('/', 'HomeController@index');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/', 'HomeController@index');      
+    });
+});
+Route::group(['prefix' => '/admin'], function () {
+    
+    Route::get('/', 'HomeController@admin');
+    
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/', 'HomeController@admin');
+    });
 });
