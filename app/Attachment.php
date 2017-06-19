8e6d5d5a8b712c8a27e8671eabf967d08e76acad
SubaruYashiro <michaell.randicha@gmail.com>
2017-06-20 01:12:51 +0700

    kenapa relationship baru jalan dan baru perhatian apa yang salah pas sblm e mau bikin, dan skrg uda jalan kabeh ._. dan ga make relation tsb

--- app/Attachment.php
+++ app/Attachment.php
@@ -9,4 +9,8 @@ class Attachment extends Model
     protected $fillable = [
         'participants_id', 'attachment_no', 'attachment_path', 'attachment_confirmed'
     ];
+
+    public function participant(){
+    	return $this->belongTo('App\Attachment', 'participants_id', 'id');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token', 'confirmation_code'
     ];
+
+    public function participate(){
+        return $this->hasMany('App\Participant', 'user_id', 'id');
+    }
 }
